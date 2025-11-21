6bc251d19bb977cc0c4c9911b6b94fe19bfec6a9
BriZmoore <bmoo5276@students.vsu.edu>
2025-11-20 22:28:50 -0500

    added bottom nav

---
+++ app/src/main/java/com/example/jellyjamsapp/SomethingNewActivity.kt
@@ -0,0 +1,11 @@
+package com.example.jellyjamsapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SomethingNewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_something_new)
+    }
+}
